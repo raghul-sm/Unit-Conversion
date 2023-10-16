fb79bb60ec9faf4e5009bbbdedf7f68689dcf590
raghul-sm <mjraghul@gmail.com>
2023-10-16 15:00:35 +0530

    Commit changes
    Removed comments

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-/*
-1 meter = 3.281 feet
-1 liter = 0.264 gallon
-1 kilogram = 2.204 pound
-*/
-//20 meters = 65.616 feet | 20 feet = 6.096 meters
-//20 liters = 5.284 gallons | 20 gallons = 75.708 liters
-//20 kilos = 44.092 pounds | 20 pounds = 9.072 kilos
-
 let inputBox = document.getElementById("input-box")
 const convertBtn = document.getElementById("convert-btn")
 const s2Result = document.getElementById("s2-result") 
